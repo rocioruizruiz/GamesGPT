3ad3afb6162431b079cc016a9b7e71d76c0fa9cd
Rocio Ruiz <rocioruizruiz10@gmail.com>
2024-04-30 15:17:37 +0200

    Minor changes

--- QR_backend.py
+++ QR_backend.py
@@ -1,11 +1,16 @@
 #import a_env_vars
 import os
+import streamlit as st
+from operator import itemgetter
+from dotenv import load_dotenv, find_dotenv
 
-#from langchain.sql_database import SQLDatabase
 from langchain_community.utilities import SQLDatabase
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_sql_query_chain
-from dotenv import load_dotenv, find_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 
 
 # 1. Cargar la bbdd con langchain
@@ -17,13 +22,13 @@ db = SQLDatabase.from_uri("mysql+pymysql://root:Zaragoza.24@localhost/SphereDB")
 load_dotenv(find_dotenv(), override=True)
 
 # 3. Crear el LLM
-llm = ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo')
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 
 # 4. Crear la cadena
 #cadena = create_sql_query_chain(llm, db)
 
 # 5. Formato personalizado de respuesta
-formato = """
+prompt = """
 Eres un asistente de IA muy inteligente, experto en identificar preguntas relevantes del usuario y convertirlas en consultas sql para generar la respuesta correcta. 
 Ademas eres un experto en MySQL. Dada una pregunta de entrada, primero crea una consulta MySQL sintácticamente correcta para ejecutarla, luego mira los resultados de la consulta y devuelve la respuesta a la pregunta de entrada.
 A menos que el usuario especifique en la pregunta un número concreto de ejemplos a obtener, consulta como máximo 5 resultados utilizando la cláusula LIMIT según MySQL. 
@@ -59,8 +64,9 @@ Los campos mas importantes de la tabla GamesMedals son: Season, Year, Championsh
 
 Contexto Específico:
 Resultados: Para preguntas sobre el resultado de una prueba en un championship responderas con los registros que cumplan con la condicion de la pregunta
-Medallero: Para preguntas sobre el ranking de medallas, considera sólo registros donde el campo Count = 1. Y da el resultado ordenado por mas medallas de oro, mas medallas de plata y mas medallas de bronce salvo que te indiquen que ordenes por el total de medallas.
-Medallistas: Para preguntas sobre los medallistas de una prueba responderas con todos los registros (Count = 0 y 1) que cumplan con la condicion de la pregunta.
+Medallero: Para preguntas sobre el medallero o ranking de medallas, considera sólo registros donde el campo Count = 1 y da el resultado ordenado por mas medallas de oro, mas medallas de plata y mas medallas de bronce salvo que te indiquen que ordenes por el total de medallas.
+Medallistas: Para preguntas sobre los medallistas de una prueba responderas con todos los registros con con Count = 0 y 1 que cumplan con la condicion de la pregunta.
+Atletas con mas medallas: Busca solo aquellos registros con EvType = "Individual" o EvType = "Team" y Count = 0.
 
 Tipos de Juegos:
 Games: 'EG' para Juegos Europeos (solo verano) y 'EYOF' para los Festivales Olímpicos de la Juventud Europea (verano e invierno).
@@ -72,27 +78,29 @@ Question: {question}
 
 # 6. Función para hacer la consulta
 
-def consulta(input_usuario):
-    from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+def consulta(input_usuario: dict) -> str:
 
-    question = formato.format(question = input_usuario)
+    question = prompt.format(question = input_usuario["query"])
     execute_query = QuerySQLDataBaseTool(db=db)
     write_query = create_sql_query_chain(llm, db)
+
+    # sanity check - this should never run
+    if write_query is None:
+        st.warning("QUERY IS NONE!!")
+
     chain = write_query | execute_query
 
-    from operator import itemgetter
-
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_core.prompts import PromptTemplate
-    from langchain_core.runnables import RunnablePassthrough
-
     answer_prompt = PromptTemplate.from_template(
-        """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+        """
+        Given the following user question, corresponding SQL query, and SQL result, answer the user question.
 
-    Question: {question}
-    SQL Query: {query}
-    SQL Result: {result}
-    Answer: """
+        Question: {question}
+        SQL Query: {query}
+        SQL Result: {result}
+        
+        Answer:  
+        """
+        # TODO (pablo): do we need structured output ?
     )
 
     answer = answer_prompt | llm | StrOutputParser()
@@ -105,22 +113,22 @@ def consulta(input_usuario):
 
     resultado= chain.invoke({"question": question})
     print(resultado)
-    return (resultado)
+    return resultado
 
 
-def consulta2(input_usuario):
-    cadena =""
-    consulta = formato.format(question = input_usuario)
-    resultado = db.run(cadena.invoke({"question": consulta}))
-    return(resultado)
+# def consulta2(input_usuario):
+#     cadena =""
+#     consulta = prompt.format(question = input_usuario)
+#     resultado = db.run(cadena.invoke({"question": consulta}))
+#     return(resultado)
 
-def consulta1(input_usuario):
-    from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+# def consulta1(input_usuario):
+#     from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 
-    consulta = formato.format(question = input_usuario)
-    execute_query = QuerySQLDataBaseTool(db=db)
-    write_query = create_sql_query_chain(llm, db)
-    cadena = write_query | execute_query
-    resultado = cadena.invoke({"question": consulta})    
-    #resultado = db.run(sentencia)
-    return(resultado)
+#     consulta = prompt.format(question = input_usuario)
+#     execute_query = QuerySQLDataBaseTool(db=db)
+#     write_query = create_sql_query_chain(llm, db)
+#     cadena = write_query | execute_query
+#     resultado = cadena.invoke({"question": consulta})    
+#     #resultado = db.run(sentencia)
+#     return(resultado)
--- QR_frontend.py
+++ QR_frontend.py
@@ -4,37 +4,44 @@ import QR_backend
 import streamlit as st
 from streamlit_chat import message
 
-st.title("EYOF Medal Standing")
-st.write("Ask anything about EYOF medal standing from 1991 to 2023.")
+
+def run_app():
+
+    st.title("EYOF Medal Standing")
+    st.write("Ask anything about EYOF medal standing from 1991 to 2023.")
 
 
+    ### NEW!!! Borrar esto si se quiere volver a lo anterior, y poner lo que hay en el archivo old.py
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-### NEW!!! Borrar esto si se quiere volver a lo anterior, y poner lo que hay en el archivo old.py
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Accept user input
+    if query := st.chat_input("Ask your questions?"):
+        st.session_state.messages.append({"role": "user", "content": query})
+        print(st.session_state.messages)
+        with st.chat_message("user"):
+            st.markdown(query)
 
-# Accept user input
-if query := st.chat_input("Ask your questions?"):
-    st.session_state.messages.append({"role": "user", "content": query})
-    print(st.session_state.messages)
-    with st.chat_message("user"):
-        st.markdown(query)
+        # Query the assistant using the latest chat history
+        result = QR_backend.consulta({"query": query, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
+        #print(result)
 
-    # Query the assistant using the latest chat history
-    result = QR_backend.consulta({"query": query, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
-    #print(result)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            full_response = result #["result"]
+            message_placeholder.markdown(full_response + "|")
+        message_placeholder.markdown(full_response)    
+        print(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        full_response = result#["result"]
-        message_placeholder.markdown(full_response + "|")
-    message_placeholder.markdown(full_response)    
-    print(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+if __name__ == "__main__":
+
+    run_app()
